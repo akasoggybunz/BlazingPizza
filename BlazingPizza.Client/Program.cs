992079bc0661b9bf55acd979b3f04e7ceda43261
Austin Foster <afoster@unionwireless.com>
2021-12-02 09:43:36 -0700

    Module 8 and revisit of module 1-4

--- BlazingPizza.Client/Program.cs
+++ BlazingPizza.Client/Program.cs
@@ -15,7 +15,7 @@ namespace BlazingPizza.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<OrderState>();
+            builder.Services.AddScoped<OrderState>(); //appState class
 
             builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
             .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
